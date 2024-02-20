45882f88c1f5fc2e8d00b0312d921388417d82ba
Nishant Dumbre <71167381+NishantDumbre@users.noreply.github.com>
2024-02-20 16:12:31 +0530

    Notes and task for RW of files in node
    Created notes for reading and writing based on latest learning and worked on a challenge/task/mini-project

---
+++ 3. Writing and reading from a file/task.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+    let message = ''
+
+    if (url === '/' && method === 'POST') {
+        const body = []
+        req.on('data', (data) => {
+            body.push(data)
+        })
+        return req.on('end', () => {
+            message = Buffer.concat(body).toString()
+            message = message.split('=')[1]
+            console.log(message)
+            res.write('<html>')
+            res.write('<head><title>Node Requests</title></head>')
+            res.write(`<body><p>${message}</p><form action="/" method="POST"><input type="text" name="home" /><button type="submit">Go to home</button></form></body>`)
+            res.write('</html>')
+            return res.end()
+        })
+    }
+})
+
+server.listen(1000)

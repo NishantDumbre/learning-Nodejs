82c107c02051f842637a10d01553a8267db638ad
Nishant Dumbre <71167381+NishantDumbre@users.noreply.github.com>
2024-02-28 22:27:06 +0530

    Post request Dynamic routes
    Added the post request controller and model to add item to our cart. Read the models/cart.js and controllers/shop.js files for info

---
+++ Express JS/8. POST req Dynamic Routes/controllers/shop.js
@@ -0,0 +1,62 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart')
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req,res,next) =>{
+  const prodId = req.body.productId   // On POST req, we get the body and we fetch the id of the product
+  Product.findById(prodId,(product)=>{    // We pass the id of the product in the findById method from ,models/product.js file. This method will help is find the product from our db/file and execute the cb once done
+    Cart.addProduct(prodId,product.price)   // We execute the cb by passing the price of the fetched product from our file along with the id
+  })
+  res.redirect('/cart')
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};

6069c29f49f79b918800d550f10230b6f2d61d96
Nishant Dumbre <71167381+NishantDumbre@users.noreply.github.com>
2024-02-28 20:00:48 +0530

    Dynamic routes to view an item
    Used the template given by the trainer as it contains ejs files which I'm skipping. Mentioned notes on new learnings, read controllers/shop.js  models/product/js and routes/shop.js

---
+++ 7. Dynamic Routes/models/product.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {   //getting all the data from the file and storing it in products.
+      const product = products.find(p => p.id === id);    // In products, we find the matching ID
+      cb(product);  // This callback will be executed only after above code is executed. We Pass a function on what to do with the arguement
+    });
+  }
+};
